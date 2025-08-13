5fbea0a393b7ac1c6cb6a1f09a64575093fa5311
germanmalykh <malykh.gn@esphere.ru>
2025-08-14 01:21:05 +0300

    TestBase update x2

--- src/test/java/config/TestBase.java
+++ src/test/java/config/TestBase.java
@@ -2,7 +2,7 @@ package config;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Allure;
+
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
