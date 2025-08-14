ad3ee1178d11b1b72fe889fc674de5d32c17cd43
germanmalykh <malykh.gn@esphere.ru>
2025-08-14 16:04:46 +0300

    Update owner

--- src/test/java/tests/RegistrationFormTest.java
+++ src/test/java/tests/RegistrationFormTest.java
@@ -18,11 +18,38 @@ public class RegistrationFormTest extends TestBase {
     RegistrationFormPage page = new RegistrationFormPage();
 
     @Test
-    @DisplayName("Заполнение формы регистрации с валидацией данных")
-    @Description("E2E тест заполнения формы регистрации с проверкой всех полей и валидацией отправленных данных")
-    @Owner("german malykh")
+    @DisplayName("Заполнение формы регистрации с валидацией данных 0")
+    @Description("E2E тест заполнения формы регистрации с проверкой всех полей и валидацией отправленных данных 1")
+    @Owner("GermanMalykh")
     @Tag("web")
-    void fillingPracticeForm() {
+    void fillingPracticeForm0() {
+        page.open()
+                .cleanupPageElements()
+                .setFirstName(UserInfo.FIRST_NAME)
+                .setLastName(UserInfo.LAST_NAME)
+                .setEmail(UserInfo.EMAIL)
+                .setGender(page.getTestData().getSelectedGender())
+                .setPhone(UserInfo.PHONE_NUMBER)
+                .setMonth(page.getTestData().getSelectedMonth())
+                .setYear(page.getTestData().getSelectedYear())
+                .setDay(page.getTestData().getSelectedDay())
+                .setSubject(page.getTestData().getSelectedSubject())
+                .setHobbies(page.getTestData().getSelectedHobbies())
+                .setPicture(PageConstants.UPLOAD_FILE_NAME)
+                .setAddress(UserInfo.ADDRESS)
+                .setState(page.getTestData().getSelectedState())
+                .setCity(page.getTestData().getSelectedCity())
+                .submitForm()
+                .checkTitle()
+                .checkTableData();
+    }
+
+    @Test
+    @DisplayName("Заполнение формы регистрации с валидацией данных 1")
+    @Description("E2E тест заполнения формы регистрации с проверкой всех полей и валидацией отправленных данных 1")
+    @Owner("GermanMalykh")
+    @Tag("web")
+    void fillingPracticeForm1() {
         page.open()
                 .cleanupPageElements()
                 .setFirstName(UserInfo.FIRST_NAME)
